a7077e500e9aafeb578ac1629881ccc6c7472bb4
Lucas Lizot <lizotllm@gmail.com>
2024-10-26 14:33:30 -0300

    fix: AAAAAAAA

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function redirectTo(platform) {
     const links = {
-      instagram: "https://www.instagram.com",
-      whatsapp: "https://wa.me/1234567890", // substitua pelo número do WhatsApp
-      email: "mailto:email@exemplo.com",
-      facebook: "https://www.facebook.com",
-      site: "https://www.seusite.com"
+      instagram: "https://www.instagram.com/cruzense_servicos/profilecard/?igsh=MTRoazhkOWNoOG5uaA%3D%3D",
+      whatsapp: "https://wa.me/5511930398666", // substitua pelo número do WhatsApp
+      email: "mailto:contato@cruzense.com.br",
+      facebook: "https://www.facebook.com/profile.php?id=100064729432294&mibextid=LQQJ4d&rdid=faXnkKnSskwrPbVX&share_url=https%3A%2F%2Fwww.facebook.com%2Fshare%2Fw7TtKzb1MCZqtztS%2F%3Fmibextid%3DLQQJ4d",
+      site: "https://www.cruzense.com.br"
     };
     
     // Redireciona para o link correspondente ao botão clicado
